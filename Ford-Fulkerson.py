f111591f4ccc3c9c166175a22efb9df533d6f2f4
MaxwellHinton <qwv9850@autuni.ac.nz>
2023-07-31 16:07:07 +1200

    Changed .json file for debugging. Implemented BFS.

--- Ford-Fulkerson.py
+++ Ford-Fulkerson.py
@@ -1,12 +1,42 @@
 import random
 
 
+class Queue:
+    def __init__(self):
+        self.elements = []
+
+    #Adds an element to the rear of the queue
+    def enqueue(self, element):
+        self.elements.append(element)
+
+    #Removes element at the front of the queue
+    def dequeue(self):
+        if not self.isEmpty():
+            return self.elements.pop(0)
+        else:
+            print("Your queue is empty.") #Raise an exception
+    
+    #Returns element at the front of the queue
+    def first(self):
+        if not self.isEmpty():
+            return self.elements[0]
+        else:
+            print("Your queue is empty.") #Raise an exception
+
+    #Returns if the queue is empty or not
+    def isEmpty(self):
+        return len(self.elements) == 0
+
+    def size(self):
+        return len(self.elements)
+
 class Node:
     def __init__(self, ID):
         self.ID = ID
         self.neighbours = [] #Neighbours store adjacent edges
         self.source = False
         self.sink = False
+        self.visited = False
 
 class Edge:
     def __init__(self, source, destination):
@@ -35,19 +65,12 @@ class Graph:
         self.edges.append(edge)
         source.neighbours.append(edge)
 
+    def get_source(self):
+        for node in self.nodes:
+            if node.source:
+                return node
 
-#Printing graph information
-def  print_graph(graph):
-
-    for node in graph.nodes:
-        print("Node " + str(node.ID) + 
-              (" (source node)" if node.source else " (sink node)" if node.sink else "") + " has neighbours: ")
-
-        for edge in node.neighbours:
-            print(f"\t{edge.destination.ID} with capacity {edge.capacity}")
-
-        print("")
-
+#Create functions
 def create_nodes(graph, total_nodes):
     i = 1
     while i <= total_nodes:
@@ -73,6 +96,48 @@ def create_edges(graph):
 
     graph.add_edge(graph.nodes[3], graph.nodes[4], capacity = 3)
 
+#Printing functions
+def  print_graph(graph):
+    for node in graph.nodes:
+        print("Node " + str(node.ID) + 
+              (" (source node)" if node.source else " (sink node)" if node.sink else "") + " has neighbours: ")
+
+        for edge in node.neighbours:
+            print(f"\t{edge.destination.ID} with capacity {edge.capacity} and flow {edge.flow}")
+
+        print("")
+
+def print_queue(queue):
+    if not queue.isEmpty():
+        for i in range(0, queue.size()):
+            node_id = queue.elements[i].ID
+            print("Node ", node_id)
+    else:
+        print("Queue empty")
+
+#Breadth-first-search
+
+def bfs(graph, source):
+    empty_queue = Queue()
+    source_node = source
+
+    empty_queue.enqueue(source_node)
+    source_node.visited = True
+    
+    while not empty_queue.isEmpty():
+        current_node = empty_queue.dequeue()
+        print("Visited Node: ", current_node.ID)
+
+        for edge in current_node.neighbours:
+            #Get the neighbours (edges) of the current node. Each edge has a destination which
+            #is the next node to be traversed to.
+            next_node = edge.destination
+
+            if not next_node.visited:
+                empty_queue.enqueue(next_node)
+                next_node.visited = True
+
+        
 
 #main body
 graph = Graph()
@@ -80,4 +145,10 @@ graph = Graph()
 create_nodes(graph, 5)
 create_edges(graph)
 
-print_graph(graph)
+bfs(graph, graph.get_source())
+
+for node in graph.nodes:
+    if node.visited:
+        print(f"Node {node.ID} has been visited.")
+    else:
+        print(f"Node {node.ID} has not been visited.")
